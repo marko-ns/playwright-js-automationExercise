ac939279a1c6add6e884cf78b350e58b298bdf0f
marko-ns <marko96jovic@gmail.com>
2023-09-04 00:32:49 +0200

    Disabled Goodle Adsense with network routing

--- tests/verifyTestCasesPage.spec.js
+++ tests/verifyTestCasesPage.spec.js
@@ -2,18 +2,13 @@ import { test } from "../fixtures";
 import { expect } from "@playwright/test";
 
 test('Verify Test Cases Page', async ({ page, homePage, testCasesPage }) => {
-    await homePage.goToHomePage()
-    //await page.pause()
-
-    await page.route("**/*", (request) => {
-        request.request().url().startsWith("https://googleads.")
-          ? request.abort()
-          : request.continue();
-        return;
-      });
-
-    await homePage.testCasesButton.click()
-    await page.pause()
-    await expect(page).toHaveURL("https://automationexercise.com/test_cases")
-    await page.pause()
+  await page.route("**/*", (request) => {
+    request.request().url().startsWith("https://googleads.")
+      ? request.abort()
+      : request.continue();
+    return;
+  });
+  await homePage.goToHomePage()
+  await homePage.testCasesButton.click()
+  await expect(page).toHaveURL("https://automationexercise.com/test_cases")
 })
